8a2cf8525f654dbd00babac369477bd148a65867
Neve Callaway <nevecallaway@gmail.com>
2025-03-15 15:08:50 -0600

    accessed web files, server up!

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -3,8 +3,9 @@ import static spark.Spark.*;
 public class Server {
     public void run(int port) {
         port(port);
+        staticFiles.location("web");
 
-        get("/", (req, res) -> "♕ 240 Chess Server is running!");
+        get("/", (req, res) -> "240 Chess Server is running!");
 
         System.out.println("Server started on http://localhost:" + port);
     }
